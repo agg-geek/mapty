b467905327dabc5f785a8f6862ebff5a48cdd4b7
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-05 22:57:49 +0530

    Implement running and cycling classes

--- script.js
+++ script.js
@@ -26,6 +26,32 @@ class Workout {
 	}
 }
 
+class Running extends Workout {
+	constructor(coords, distance, duration, cadence) {
+		super(coords, distance, duration);
+		this.cadence = cadence;
+		this.calcPace();
+	}
+
+	calcPace() {
+		this.pace = this.duration / this.distance; // mins/km
+		return this.pace;
+	}
+}
+
+class Cycling extends Workout {
+	constructor(coords, distance, duration, elevationGain) {
+		super(coords, distance, duration);
+		this.elevationGain = elevationGain;
+		this.calcSpeed();
+	}
+
+	calcSpeed() {
+		this.speed = this.distance / (this.duration / 60); // km/hr
+		return this.speed;
+	}
+}
+
 // =====================================================================
 
 const form = document.querySelector('.form');
