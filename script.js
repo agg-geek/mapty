abe1f3ede863f8dc92cf313b05986bac22724507
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-05 18:40:53 +0530

    Use Geolocation API to get location

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -10,3 +22,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// this does not work through a proxy
+navigator.geolocation.getCurrentPosition(
+	// success callback
+	function (position) {
+		// console.log(position);
+		const { latitude, longitude } = position.coords;
+		console.log(latitude, longitude);
+	},
+	// failure callback
+	function () {
+		alert('Could not get your location coordinates!');
+		throw new Error('App quit because location could not be found');
+	}
+);
